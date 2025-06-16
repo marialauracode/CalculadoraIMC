4826fe1aa56092ef6a084bcdd749a98d456e7338
Maria Laura Castro <cmarialaura599@gmail.com>
2025-06-16 12:01:33 -0300

    JS

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 function calcular() {
     var inputAltura = document.getElementById('inputAltura')
-    var valorAltura = inputAltura.value 
+    var valorAltura = inputAltura.value
 
     var inputPeso = document.getElementById('inputPeso')
-    var valorPeso = inputPeso.value 
+    var valorPeso = inputPeso.value
 
-    var calculoIMC = valorPeso / (valorAltura**2)
+    var calculoIMC = valorPeso / (valorAltura ** 2)
 
-    if (calculoIMC <= 18.5) {
-        var classificacaoIMC = 'MAGREZA'
-    } else if (calculoIMC >= 18.6 && calculoIMC <= 24.9) {
-        classificacaoIMC = 'NORMAL'
-    } else if (calculoIMC >=25 && calculoIMC <= 30) {
-        classificacaoIMC = 'SOBREPESO'
-    } else if (calculoIMC >= 30.1 && calculoIMC <= 40) {
-        classificacaoIMC = 'OBESIDADE'
+    if (isNaN(valorAltura) && isNaN(valorPeso)) {
+        alert('Entradas inválidas! Tente novamente')
+        document.getElementById('resultadoIMC').innerText = ""
+
+    } else {
+        if (calculoIMC <= 18.5) {
+            var classificacaoIMC = 'MAGREZA'
+            document.getElementById('resultadoIMC').innerHTML = 'Altura: ' + valorAltura + ' metros <br>' + 'Peso: ' + valorPeso + 'kg <br>' +
+                'Classificação: <span style="color:#96A3AB; font-weight:700">' + calculoIMC.toFixed(2) + ' (' + classificacaoIMC + ')</span>'
+        } else if (calculoIMC >= 18.6 && calculoIMC <= 24.9) {
+            classificacaoIMC = 'NORMAL'
+            document.getElementById('resultadoIMC').innerHTML = 'Altura: ' + valorAltura + ' metros <br>' + 'Peso: ' + valorPeso + 'kg <br>' +
+                'Classificação: <span style="color:#0AAF68; font-weight:700">' + calculoIMC.toFixed(2) + ' (' + classificacaoIMC + ')</span>'
+        } else if (calculoIMC >= 25 && calculoIMC <= 30) {
+            classificacaoIMC = 'SOBREPESO'
+            document.getElementById('resultadoIMC').innerHTML = 'Altura: ' + valorAltura + ' metros <br>' + 'Peso: ' + valorPeso + 'kg <br>' +
+                'Classificação: <span style="color:#E2B037; font-weight:700">' + calculoIMC.toFixed(2) + ' (' + classificacaoIMC + ')</span>'
+        } else if (calculoIMC >= 30.1 && calculoIMC <= 40) {
+            classificacaoIMC = 'OBESIDADE'
+            document.getElementById('resultadoIMC').innerHTML = 'Altura: ' + valorAltura + ' metros <br>' + 'Peso: ' + valorPeso + 'kg <br>' +
+                'Classificação: <span style="color:#C3413F; font-weight:700">' + calculoIMC.toFixed(2) + ' (' + classificacaoIMC + ')</span>'
+        } else if (calculoIMC > 40) {
+            classificacaoIMC = 'OBESIDADE GRAVE'
+            document.getElementById('resultadoIMC').innerHTML = 'Altura: ' + valorAltura + ' metros <br>' + 'Peso: ' + valorPeso + 'kg <br>' +
+                'Classificação: <span style="color:#530F0A; font-weight:700">' + calculoIMC.toFixed(2) + ' (' + classificacaoIMC + ')</span>'
+        }
+
     }
 
-    document.getElementById('resultadoIMC').innerText = 'Altura: ' + valorAltura + ' metros \n' + 'Peso: ' + valorPeso + 'kg \n' + 
-    'Classificação: ' + calculoIMC.toFixed(2) + ' (' + classificacaoIMC + ')'
+    inputAltura.value = ""
+    inputPeso.value = ""
+
 
 }
